fe8f8d69bc7105fbe8650670da7bda2d4a64b42c
Vbobell <vinicius.leite@gupy.com.br>
2023-02-22 15:13:38 -0300

    chore(gateway): add proxy to swapi

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    '/swapi',
+    createProxyMiddleware({
+      target: 'http://localhost:3001',
+      changeOrigin: true,
+      pathRewrite: {
+        [`^/swapi`]: '',
+      },
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
