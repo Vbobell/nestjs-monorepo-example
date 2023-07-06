c8c9c9187743a8a9b752ebe823b37d819abe454b
Vbobell <vinicius.leite@gupy.com.br>
2023-07-06 18:43:04 -0300

    chore(abstract-tools): create abstract use case

---
+++ libs/abstract-tools/src/application/use-case/abstract.use-case.unit.spec.ts
@@ -0,0 +1,35 @@
+import {
+  DomainParam,
+  DomainReturn,
+  MakeUseCase,
+} from '@libs/abstract-tools/application/use-case/abstract.use-case.fixture';
+
+describe('MakeUseCase', () => {
+  let domainParam: DomainParam;
+  let domainReturn: DomainReturn;
+  let makeUseCase: MakeUseCase;
+
+  beforeEach(() => {
+    domainParam = {
+      domainParamStr: 'fake-str',
+      domainParamBoolean: true,
+      domainParamNumber: 1,
+    };
+
+    domainReturn = {
+      domainReturnStr: 'fake-str',
+      domainReturnBoolean: true,
+      domainReturnNumber: 1,
+    };
+
+    makeUseCase = new MakeUseCase();
+  });
+
+  describe('When implements UseCase', () => {
+    test('Then execute with success', () => {
+      const result = makeUseCase.execute(domainParam);
+
+      expect(result).toEqual(domainReturn);
+    });
+  });
+});
