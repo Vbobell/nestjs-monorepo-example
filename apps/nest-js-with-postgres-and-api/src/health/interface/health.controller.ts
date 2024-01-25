14fcf9757a78fef346a8c8ba0c924e0ae01e54f8
Vbobell <vinicius.bobel41@gmail.com>
2024-01-25 15:17:09 -0300

    fix(nest-with-postgres-and-api): refactor health controller
    remove config service and log

--- apps/nest-js-with-postgres-and-api/src/health/interface/health.controller.ts
+++ apps/nest-js-with-postgres-and-api/src/health/interface/health.controller.ts
@@ -1,5 +1,4 @@
 import { Controller, Get, HttpCode } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { Observable, of } from 'rxjs';
 
 import { Health, HealthCheckUseCase } from '@libs/health-check';
@@ -9,16 +8,11 @@ export class HealthController {
   private readonly NAME = 'nest-js-with-postgres-and-api-service';
   private readonly DESCRIPTION = 'Example to create API with postgres';
 
-  constructor(
-    private readonly healthCheckUseCase: HealthCheckUseCase,
-    private readonly configService: ConfigService,
-  ) {}
+  constructor(private readonly healthCheckUseCase: HealthCheckUseCase) {}
 
   @Get()
   @HttpCode(200)
   health(): Observable<Health> {
-    console.log(this.configService.get('database'));
-
     return of(this.healthCheckUseCase.execute(this.DESCRIPTION, this.NAME));
   }
 }
